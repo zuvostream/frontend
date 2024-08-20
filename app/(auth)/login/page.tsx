872d4b3366160288fb19627450974b504bb6c6d5
sownyluv <snowyfe@proton.me>
2024-08-20 10:24:00 -0500

    Discord

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -5,20 +5,16 @@ import { Input } from "@/components/ui/input";
 import Link from "next/link";
 import { FaApple, FaDiscord, FaSpotify} from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
-export default function Register() {
+export default function Login() {
     let [username, setUsername] = useState<string>('');
-    let [email, setEmail] = useState<string>('');
     let [password, setPassword] = useState<string>('');
      
     return(
         <div className="flex min-h-screen justify-center items-center">
                 <div className="border rounded-xl h-auto w-[350px] px-8 py-8 flex flex-col gap-4">
                     <div className="flex-rol flex gap-4 w-full ">
-                    <Button className="w-full" variant={'outline'} size={"lg"}>
-                        <FaDiscord className="h-6 w-6" />
-                    </Button>
-                    <Button className="w-full" variant={'outline'} size={"lg"}>
-                        <FaSpotify className="h-6 w-6" />
+                    <Button className="w-full" variant={'outline'} size={"lg"} asChild>
+                       <Link href=''><FaDiscord className="h-6 w-6" /> </Link>
                     </Button>
                     <Button className="w-full" disabled variant={'outline'} size={"lg"}>
                         <FaApple className="h-6 w-6" />
@@ -29,16 +25,9 @@ export default function Register() {
                         <Input type="text" value={username} onChange={(e) => setUsername(e.target.value)} required />
                     </div>
                     <div className="space-y-2">
-                        <p className="text-base">Email<span className="text-red-500 select-none font-bold">*</span></p>
-                        <Input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-                    </div>
-                    <div className="space-y-2">
                         <p className="text-base">Password<span className="text-red-500 select-none font-bold">*</span></p>
                         <Input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
                     </div>
-                    <div>
-                    <span className="text-xs">By registering you agree to our <Link className="hover:text-primary transition-all font-semibold" href='/'>Terms Of Service</Link> </span>
-</div>
                     <Button variant={"default"}>
                         Register
                     </Button>
